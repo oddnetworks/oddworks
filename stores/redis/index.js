23be65d7943011dd62595db0b1da852e4a775c6b
Blain Smith <rebelgeek@blainsmith.com>
2016-04-02 14:38:48 -0400

    redis store refactor

--- stores/redis/index.js
+++ stores/redis/index.js
@@ -1,20 +1,55 @@
 'use strict';
 
+const _ = require('lodash');
 const Promise = require('bluebird');
 const uuid = require('uuid');
 
 const store = exports = module.exports = {};
+let config = {};
 
 store.initialize = (bus, options) => {
-	this.bus = bus;
-	this.options = options;
+	config.bus = bus;
+	config.options = options;
 
-	if (this.options.types) {
-		this.options.types.forEach(type => {
-			bus.queryHandler({role: 'store', cmd: 'get', type: type}, payload => {
-				return Promise.resolve(payload);
+	Promise.promisifyAll(config.options.redis);
+
+	return new Promise((resolve, reject) => {
+		if (config.options.types) {
+			config.options.types.forEach(type => {
+				bus.queryHandler({role: 'store', cmd: 'get', type}, get);
+				bus.queryHandler({role: 'store', cmd: 'set', type}, set);
 			});
-		});
-	}
+
+			resolve(true);
+		} else {
+			reject(new Error('options.types is missing'));
+		}
+	});
 };
+
+function get(payload) {
+	return new Promise((resolve) => {
+		if (payload.id) {
+			return config.options.redis.hgetAsync(payload.type, payload.id)
+				.then(object => {
+					return resolve(JSON.parse(object));
+				});
+		}
+
+		return config.options.redis.hgetallAsync(payload.type)
+			.then(objects => {
+				objects = _.map(_.toArray(objects), object => {
+					return JSON.parse(object);
+				});
+				return resolve(objects);
+			});
+	});
+}
+
+function set(payload) {
+	return new Promise((resolve) => {
 		payload.id = payload.id || uuid.v4();
+		config.options.redis.hsetAsync(payload.type, payload.id, JSON.stringify(payload));
+		return resolve(true);
+	});
+}
