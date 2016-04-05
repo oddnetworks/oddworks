122ebd63cf1169b011cee9301bc5b9a3775693dd
Blain Smith <rebelgeek@blainsmith.com>
2016-04-04 20:19:38 -0400

    generate device and user token

--- cli/index.js
+++ cli/index.js
@@ -8,13 +8,28 @@ const jwt = require('jsonwebtoken');
 const chalk = require('chalk');
 
 commander
-	.command('generate-token <network> <device>')
+	.command('generate-device-token <network> <device>')
 	.action((network, device) => {
 		const payload = {
 			version: 1,
 			network,
 			device,
 			scope: ['device']
+		};
+		const token = jwt.sign(payload, process.env.JWT_SECRET);
+		console.log('### JWT ###');
+		console.log(chalk.green(token));
+	});
+
+commander
+	.command('generate-user-token <network> <device> <user>')
+	.action((network, device, user) => {
+		const payload = {
+			version: 1,
+			network,
+			device,
+			user,
+			scope: ['user']
 		};
 		const token = jwt.sign(payload, process.env.JWT_SECRET);
 		console.log('### JWT ###');
