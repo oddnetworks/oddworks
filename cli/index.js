ed396c99bc102ab9be833ccbf7334fd53814109d
Patrick McConnell <mcconnellpatrick@gmail.com>
2016-04-15 12:02:16 -0400

    add token list CLI command

--- cli/index.js
+++ cli/index.js
@@ -8,4 +8,5 @@ const commander = require('commander');
 commander
 	.usage('[cmd]')
 	.command('generate-token', 'Generate a device JWT for the network and device ids specified.')
+	.command('token-list', 'List currently available JWT Tokens')
 	.parse(process.argv);
