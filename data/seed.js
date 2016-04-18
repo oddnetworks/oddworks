32056e42328a53cccb78cf04ad1e9f47e598356d
Blain Smith <rebelgeek@blainsmith.com>
2016-04-18 10:48:08 -0400

    better coloring

--- data/seed.js
+++ data/seed.js
@@ -11,38 +11,64 @@ const jwt = require('jsonwebtoken');
 
 const isDev = (process.env.NODE_ENV === 'development');
 
+function loadFiles(files) {
+	return _.map(files, file => {
+		return require(path.join(__dirname, file)); // eslint-disable-line
+	});
+}
+
+function seedData(bus, objects) {
+	return _.map(objects, object => {
+		const searchable = Boolean(_.indexOf(searchableTypes, object.type) + 1);
+		let pattern = {role: 'store', cmd: 'set', type: object.type};
+		if (searchable) {
+			pattern = {role: 'catalog', cmd: 'create', searchable: true};
+		}
+		if (isDev) {
+			const payload = {
+				version: 1,
+				channel: object.channel,
+				platform: object.id,
+				scope: ['platform']
+			};
+
+			const token = jwt.sign(payload, process.env.JWT_SECRET);
+			if (object.type === 'platform') {
+				console.log(chalk.blue(`${_.capitalize(object.type)}: `) + chalk.cyan(object.id));
+				console.log(chalk.blue('     JWT: ') + chalk.cyan(token));
+				console.log('');
+			} else {
+				console.log(chalk.blue(`${_.capitalize(object.type)}: `) + chalk.cyan(object.id));
+			}
+			if (object.type === 'channel') {
+				console.log('');
+			}
+		}
+		return bus.sendCommand(pattern, object);
+	});
+}
+
 module.exports = bus => {
-	return glob('./**/*.json', {cwd: __dirname})
-		.then(files => {
-			return _.map(files, file => {
-				return require(path.join(__dirname, file)); // eslint-disable-line
-			});
-		})
+	return glob('./+(channel|platform)/*.json', {cwd: __dirname})
+		.then(loadFiles)
 		.then(objects => {
 			if (isDev) {
 				console.log('');
-				console.log(chalk.blue(`Loading test data...`));
+				console.log(chalk.green(`Loading test Channel and Platforms...`));
+				console.log(chalk.green(`-------------------------------------`));
 			}
-			return Promise.all(
-				_.map(objects, object => {
-					const searchable = Boolean(_.indexOf(searchableTypes, object.type) + 1);
-					let pattern = {role: 'store', cmd: 'set', type: object.type};
-					if (searchable) {
-						pattern = {role: 'catalog', cmd: 'create', searchable: true};
-					}
-					if (isDev) {
-						const payload = {
-							version: 1,
-							channel: object.channel,
-							platform: object.id,
-							scope: ['platform']
-						};
-
-						const token = jwt.sign(payload, process.env.JWT_SECRET);
-						console.log(chalk.blue(`${_.capitalize(object.type)}: ${object.id} ${((object.type === 'platform') ? token : '')}`));
-					}
-					return bus.sendCommand(pattern, object);
-				})
-			);
+			return Promise.all(seedData(bus, objects));
+		})
+		.then(() => {
+			return glob('./+(collection|promotion|video|view)/*.json', {cwd: __dirname});
+		})
+		.then(loadFiles)
+		.then(objects => {
+			if (isDev) {
+				console.log('');
+				console.log(chalk.green(`Loading test Resources...`));
+				console.log(chalk.green(`-------------------------`));
+			}
+			return Promise.all(seedData(bus, objects));
 		});
 };
