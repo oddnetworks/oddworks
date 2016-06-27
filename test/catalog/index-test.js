6a2b58e67e860651ce50b49ae2589f6fd2e8512f
Kristoffer Walker <kris@kixx.name>
2016-06-27 11:58:31 -0400

    Remove deprecated tape tests

--- test/catalog/index-test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const test = require('tape');
-
-test('CATALOG', t => {
-	t.skip('Unit test catalog service, not server');
-	t.end();
-});
--- test/identity-test.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const test = require('tape');
-
-test('IDENTITY', t => {
-	t.skip('Unit test identity service, not server');
-	t.end();
-});
