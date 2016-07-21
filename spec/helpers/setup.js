2a4d27a6b34e57b84be73d8c25ce9b65504438c2
Kristoffer Walker <kris@kixx.name>
2016-07-21 10:18:26 -0400

    Monkey patch MockServerResponse to fix Buffer.concat()
    	modified:   spec/helpers/setup.js

--- spec/helpers/setup.js
+++ spec/helpers/setup.js
@@ -2,9 +2,22 @@
 /* eslint prefer-arrow-callback: 0 */
 'use strict';
 
+const MockServerResponse = require('mock-express-response/node_modules/mock-res');
 const support = require('../support/');
 
 const SETUP_TIMEOUT = 5000;
+
+MockServerResponse.prototype._getString = function () {
+	const buffs = this._readableState.buffer;
+
+	if (Array.isArray(buffs)) {
+		return buffs.map(buff => {
+			return buff.toString();
+		}).join('');
+	}
+
+	return buffs.toString();
+};
 
 beforeAll(function (done) {
 	this.createBus = support.createBus;
