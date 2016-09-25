50f87ca4d3b232132cce7c12180d0d28c768f5c1
Al Jachimiak <aljachimiak@gmail.com>
2016-09-25 14:39:45 -0400

    starts tests for POST of catalog items

---
+++ spec/services/catalog/controllers/admin-catalog-list-controller-spec.js
@@ -0,0 +1,96 @@
+/* global describe, beforeAll, xdescribe */
+/* eslint prefer-arrow-callback: 0 */
+/* eslint-disable max-nested-callbacks */
+'use strict';
+
+const Promise = require('bluebird');
+const fakeredis = require('fakeredis');
+const redisStore = require('../../../../lib/stores/redis/');
+const identityService = require('../../../../lib/services/identity');
+const catalogService = require('../../../../lib/services/catalog');
+
+describe('Catalog Service Controller', function () {
+	let bus;
+
+	const CHANNEL = {
+		id: 'odd-networks',
+		type: 'channel',
+		title: 'Odd Networks'
+	};
+
+	const PLATFORM = {
+		id: 'apple-ios',
+		type: 'platform',
+		title: 'Apple iOS',
+		channel: 'odd-networks'
+	};
+
+	const USER = {
+		id: 'user-id',
+		type: 'user',
+		title: 'User',
+		channel: 'odd-networks'
+	};
+
+	const COLLECTION = {
+		id: 'collection-13',
+		type: 'collection',
+		title: 'Collection 13',
+		channel: 'odd-networks'
+	};
+
+	const VIDEO = {
+		id: 'video-13',
+		type: 'video',
+		title: 'Video 13',
+		channel: 'odd-networks'
+	};
+
+	beforeAll(function (done) {
+		bus = this.createBus();
+		this.service = null;
+
+		Promise.promisifyAll(fakeredis.RedisClient.prototype);
+		Promise.promisifyAll(fakeredis.Multi.prototype);
+
+		// Initialize a store
+		redisStore(bus, {
+			types: ['channel', 'platform', 'user', 'collection', 'video'],
+			redis: fakeredis.createClient()
+		})
+		.then(store => {
+			this.store = store;
+		})
+		// Initialize an identity service
+		.then(() => {
+			return identityService(bus, {});
+		})
+		// Initialize a catalog service
+		.then(() => {
+			return catalogService(bus, {});
+		})
+		.then(service => {
+			this.service = service;
+			this.controller = {
+				collection: new service.CatalogItemController({bus, type: 'collection'}),
+				video: new service.CatalogItemController({bus, type: 'video'})
+			};
+		})
+		.then(() => {
+			return Promise.join(
+				bus.sendCommand({role: 'store', cmd: 'set', type: 'channel'}, CHANNEL),
+				bus.sendCommand({role: 'store', cmd: 'set', type: 'platform'}, PLATFORM),
+				bus.sendCommand({role: 'store', cmd: 'set', type: 'user'}, USER),
+				bus.sendCommand({role: 'catalog', cmd: 'setItem', type: 'collection'}, COLLECTION),
+				bus.sendCommand({role: 'catalog', cmd: 'setItem', type: 'video'}, VIDEO),
+				() => {}
+			);
+		})
+		.then(done)
+		.catch(done.fail);
+	});
+
+	xdescribe('Admin POST adds a collection object');
+
+	xdescribe('Admin POST adds a video object');
+});
