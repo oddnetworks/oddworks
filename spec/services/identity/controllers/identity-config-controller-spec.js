fec4ab189e33b630a12d43ea998827a04e72c5aa
Kristoffer Walker <kris@kixx.name>
2016-10-18 06:40:11 -0400

    Adds test stub for Identity Config Controller
    	new file:   spec/services/identity/controllers/identity-config-controller-spec.js

---
+++ spec/services/identity/controllers/identity-config-controller-spec.js
@@ -0,0 +1,9 @@
+/* global xdescribe it */
+/* eslint prefer-arrow-callback: 0 */
+/* eslint-disable max-nested-callbacks */
+'use strict';
+
+xdescribe('Identity Config Controller', function () {
+	it('should be tested');
+});
+
