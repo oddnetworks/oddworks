5d7dfea810da041c9f7c0cabb44f61724d7396be
Kristoffer Walker <kris@kixx.name>
2016-10-27 07:09:38 -0400

    Adds pending tests for Viewer Controllers.
    	new file:   spec/services/identity/controllers/identity-viewer-controller-spec.js
    	new file:   spec/services/identity/controllers/identity-viewers-list-controller-spec.js

---
+++ spec/services/identity/controllers/identity-viewer-controller-spec.js
@@ -0,0 +1,35 @@
+/* global describe, it */
+/* eslint prefer-arrow-callback: 0 */
+/* eslint-disable max-nested-callbacks */
+'use strict';
+
+// Reference identity-item-controller-spec.js and identity-channel-controller-spec.js
+
+describe('Identity Viewer Controller', function () {
+	describe('GET', function () {
+		describe('as platform', function () {
+			it('needs to be tested');
+		});
+		describe('as admin', function () {
+			it('needs to be tested');
+		});
+	});
+
+	describe('PATCH', function () {
+		describe('as platform', function () {
+			it('needs to be tested');
+		});
+		describe('as admin', function () {
+			it('needs to be tested');
+		});
+	});
+
+	describe('DELETE', function () {
+		describe('as platform', function () {
+			it('needs to be tested');
+		});
+		describe('as admin', function () {
+			it('needs to be tested');
+		});
+	});
+});
